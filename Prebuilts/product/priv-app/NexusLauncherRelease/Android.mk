389e01c38aabc3230b220bd19aa7556b27b4c94d
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2023-01-25 20:24:55 +0530

    Build fullmod2z variant

--- Prebuilts/product/priv-app/NexusLauncherRelease/Android.mk
+++ Prebuilts/product/priv-app/NexusLauncherRelease/Android.mk
@@ -18,33 +18,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := NexusLauncherReleaseMod
-ifeq ($(PIXEL_LAUNCHER_VARIANT),extragrids)
-LOCAL_SRC_FILES := NexusLauncherRelease10.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),glance1)
-LOCAL_SRC_FILES := NexusLauncherRelease01.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),glance2)
-LOCAL_SRC_FILES := NexusLauncherRelease02.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),fullmod1)
-LOCAL_SRC_FILES := NexusLauncherRelease11.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),fullmod2)
-LOCAL_SRC_FILES := NexusLauncherRelease12.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),nomod)
-LOCAL_SRC_FILES := NexusLauncherRelease00.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),extragridsz)
-LOCAL_SRC_FILES := NexusLauncherRelease10z.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),glance1z)
-LOCAL_SRC_FILES := NexusLauncherRelease01z.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),glance2z)
-LOCAL_SRC_FILES := NexusLauncherRelease02z.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),fullmod1z)
-LOCAL_SRC_FILES := NexusLauncherRelease11z.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),fullmod2z)
 LOCAL_SRC_FILES := NexusLauncherRelease12z.apk
-else ifeq ($(PIXEL_LAUNCHER_VARIANT),nomodz)
-LOCAL_SRC_FILES := NexusLauncherRelease00z.apk
-else
-$(error Not set PIXEL_LAUNCHER_VARIANT, read more in vendor/PixelLauncherExtended/codename.txt)
-endif
 LOCAL_PRODUCT_MODULE := true
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
